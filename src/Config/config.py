fa0e02639674d0a535c00bf9707e80dd16bb4a03
Edgar <andradeed78@yahoo.com>
2025-05-27 13:13:47 -0500

    fixed parameter optimization

--- src/Config/config.py
+++ src/Config/config.py
@@ -2,11 +2,13 @@ from pathlib import Path
 
 PATHS = {
     'human_data':Path(Path.cwd(), '..', 'data', 'human').resolve(),
+    'simulated_data':Path(Path.cwd(), '..', 'data', 'simulated').resolve(),
     'parameter_fit_results':Path.cwd() / Path('..').resolve() / Path('reports'),
     'index_path':Path(Path.cwd(), '..', 'data', 'indices').resolve()
 }
 
 # Chech if the paths exist
 PATHS['human_data'].mkdir(parents=True, exist_ok=True)
+PATHS['simulated_data'].mkdir(parents=True, exist_ok=True)
 PATHS['parameter_fit_results'].mkdir(parents=True, exist_ok=True)
 PATHS['index_path'].mkdir(parents=True, exist_ok=True)
--- src/Utils/parameter_optimization.py
+++ src/Utils/parameter_optimization.py
@@ -12,7 +12,7 @@ from Utils.interaction import Episode
 from Utils.utils import PPT
 
 
-class GetMeasure:
+class GetEfficiency:
     '''
     Class to obtain a measure from a model given some 
     free parameters.
@@ -20,46 +20,23 @@ class GetMeasure:
     Input:
         - agent_class, an agent class
         - fixed_parameters, a dictionary with the model's fixed parameters
-        - free_parameters, a dictionary with the model's free parameters
     '''
     def __init__(
                 self, 
                 agent_class: CogMod, 
-                free_parameters: Dict[str, any],
                 fixed_parameters: Dict[str, any],
-                measure: Optional[Union[str, None]]=None,
+                simulation_parameters: Dict[str, any],
+                T: Optional[int]=20
             ) -> None:
         self.agent_class = agent_class
-        self.free_parameters = free_parameters
         self.fixed_parameters = fixed_parameters
-        self.num_rounds = 100
-        self.num_episodes = 50
-        self.T = 20
-        # Select measure
-        if measure is None:
-            self.measure = 'efficiency'
-        else:
-            self.measure = measure
+        self.num_rounds = simulation_parameters['num_rounds']
+        self.num_episodes = simulation_parameters['num_episodes']
+        self.T = T
         # For debugging
         self.debug = False
 
-    def create_loss_function(self, method_measure: str) -> Callable:
-        assert(hasattr(self, method_measure))
-        arguments = ', '.join([f'{key}=None' for key in self.free_parameters.keys()])
-        function_definition = f'''
-from types import MethodType
-
-def def_funct(self, {arguments}):
-    parameters = locals()
-    measure_value = self.{method_measure}(parameters)
-    return measure_value
-
-self.black_box_function = MethodType(def_funct, self)
-'''
-        # print(function_definition)
-        exec(function_definition)
-
-    def efficiency(self, free_parameters: Dict[str, any]) -> float:
+    def efficiency(self, **free_parameters) -> float:
         # Generate simulated data
         if self.debug:
             print('Parameters for simulation:')
@@ -102,21 +79,6 @@ self.black_box_function = MethodType(def_funct, self)
             num_agents=self.fixed_parameters['num_agents'],
             threshold=self.fixed_parameters['threshold']
         )
-        fixed_parameters = self.fixed_parameters.copy()
-        fixed_parameters['num_agents']
-        if self.agent_class == MFP:
-            states = list(product([0,1], repeat=self.fixed_parameters['num_agents']))
-            count_states = ProxyDict(
-                keys=states,
-                initial_val=0
-            )
-            count_transitions = ProxyDict(
-                keys=list(product(states, repeat=2)),
-                initial_val=0
-            )
-            fixed_parameters['states'] = states
-            fixed_parameters['count_states'] = count_states
-            fixed_parameters['count_transitions'] = count_transitions
         agents = [
             self.agent_class(
                 free_parameters=free_parameters, 
@@ -124,7 +86,6 @@ self.black_box_function = MethodType(def_funct, self)
                 n=n
             ) for n in range(self.fixed_parameters['num_agents'])
         ]
-        agents[0].designated_agent = True
         return bar, agents
 
 
@@ -151,42 +112,24 @@ class ParameterOptimization :
     def __init__(
                 self, 
                 agent_class: CogMod, 
-                free_parameters: Dict[str, any],
                 fixed_parameters: Dict[str, any],
-                measure_class: any, 
-                measure: str, 
-                optimizer_name: Union[BayesianOptimization, None],
+                simulation_parameters: Dict[str, any],
                 hyperparameters: Optional[Union[Dict[str,any],None]]=None
             ) -> None:
         # --------------------------
-        # Checking measure class
+        # Checking bounds are given
         # --------------------------
-        try:
-            pr = measure_class(
-                agent_class=agent_class,
-                free_parameters=free_parameters,
-                fixed_parameters=fixed_parameters
-            )
-        except Exception as e:
-            print(f'\t{e}')
-            raise Exception('Error: measure_class should initialize with "agent_class", "free_parameters" and "fixed_parameters"!')
-        assert(hasattr(measure_class, 'create_loss_function'))
-        self.measure_class = measure_class
+        assert hasattr(agent_class, 'bounds')
         # --------------------------
         # Bookkeeping
         # --------------------------
-        self.free_parameters = free_parameters
         self.fixed_parameters = fixed_parameters
+        self.simulation_parameters = simulation_parameters
         self.agent_class = agent_class
-        self.measure = measure
+        self.verbose = False
         # --------------------------
         # Initialize optimizer
         # --------------------------
-        self.optimizer_name = optimizer_name
-        if optimizer_name == 'bayesian':
-            self.optimizer = self.create_bayesian_optimizer()
-        else:
-            raise Exception('Oooops')
         if hyperparameters is None:
                 self.hyperparameters = {
                     'init_points':4,
@@ -194,7 +137,7 @@ class ParameterOptimization :
                 }
         else:
             self.hyperparameters = hyperparameters
-        self.verbose = False
+        self.create_bayesian_optimizer()
 
     def get_optimal_parameters(self) -> Tuple[float]:
         '''
@@ -203,83 +146,24 @@ class ParameterOptimization :
         self.optimizer.maximize(**self.hyperparameters)
         return self.optimizer.max
 
-    def create_bayesian_optimizer(self):
+    def create_bayesian_optimizer(self) -> None:
         # Initialize function to get deviance from model        
-        pr = self.measure_class(
+        pr = GetEfficiency(
             agent_class=self.agent_class,
-            free_parameters=self.free_parameters,
-            fixed_parameters=self.fixed_parameters
+            fixed_parameters=self.fixed_parameters,
+            simulation_parameters=self.simulation_parameters
         )
-        pr.create_loss_function(self.measure)
         # Bounded region of parameter space
-        pbounds = self.get_pbounds()
-        print('pbounds:', pbounds)
+        pbounds = self.agent_class.bounds(self.fixed_parameters)
+        if self.verbose:
+            print('pbounds:', pbounds)
         # Initialize optimizer
         optimizer = BayesianOptimization(
-            f=pr.black_box_function,
+            f=pr.efficiency,
             pbounds=pbounds,
             random_state=1,
             allow_duplicate_points=True,
             verbose=False
         )
-        return optimizer
-    
-    def get_pbounds(self) -> Dict[str, Tuple[float]]:
-        '''Returns the bounds of the parameters'''
-        pbounds = dict()
-        if self.agent_class != MFP:
-            for parameter in self.free_parameters:
-                extend_dict = self.agent_class.bounds(self.fixed_parameters)
-                # extend_dict = self.get_saved_bounds(parameter)
-                if extend_dict is not None:
-                    pbounds.update(extend_dict)
-        
-    def get_saved_bounds(self, parameter:str) -> Tuple[float]:
-        '''Returns the known bounds for given parameter'''
-        #------------------------------------------
-        # Random
-        #------------------------------------------
-        if parameter == 'go_prob':
-            return {'go_prob':(0, 1)}
-        #------------------------------------------
-        # MFP and all other cognitive models
-        #------------------------------------------
-        elif parameter == 'inverse_temperature':
-            return {'inverse_temperature':(1, 64)}
-        #------------------------------------------
-        # WSLS
-        #------------------------------------------
-        if parameter == 'go_drive':
-            return {'go_drive':(0, 1)}
-        if parameter == 'wsls_strength':
-            return {'wsls_strength':(0, 10)}
-        #------------------------------------------
-        # PRW
-        #------------------------------------------
-        if parameter == 'initial_reward_estimate_go':
-            return {'initial_reward_estimate_go':(0, 1)}
-        if parameter == 'initial_reward_estimate_no_go':
-            return {'initial_reward_estimate_no_go':(0, 1)}
-        if parameter == 'learning_rate':
-            return {'learning_rate':(0, 1)}
-        #------------------------------------------
-        # ARW
-        #------------------------------------------
-        if parameter == 'initial_luft_estimate':
-            return {'initial_luft_estimate':(0, 1)}
-        if parameter == 'learning_rate':
-            return {'learning_rate':(0, 1)}
-        #------------------------------------------
-        # Qlearning
-        #------------------------------------------
-        if parameter == 'go_drive':
-            return {'go_drive':(0, 1)}
-        if parameter == 'discount_factor':
-            return {'discount_factor':(0, 1)}
-        if parameter == 'learning_rate':
-            return {'learning_rate':(0, 1)}
-        else:
-            raise Exception(f'Parameter {parameter} not known!')
-
-
+        self.optimizer = optimizer
 
